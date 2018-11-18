266fec2ad26df373e751d4a8d6b874ae0582fafa
elthran <jacobbrunner@gmail.com>
2018-11-17 20:06:06 -0800

    Fixed Marlen's giant mistake

--- undyingkingdoms/routes/reset_schema.py
+++ undyingkingdoms/routes/reset_schema.py
@@ -5,8 +5,8 @@ from flask_login import login_required
 from undyingkingdoms import app, db
 
 
-@app.route("/reset_schema")
 @login_required
+@app.route("/reset_schema")
 def reset_schema():
     from flask_login import current_user
     # if current_user.name not in ['...']:
