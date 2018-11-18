1dd6803a3bdbb93c822f79991eae6a47c0873312
Marlen Brunner <klondikemarlen@gmail.com>
2018-11-17 19:45:57 -0800

    Debug reset_schema route.

--- undyingkingdoms/routes/reset_schema.py
+++ undyingkingdoms/routes/reset_schema.py
@@ -6,9 +6,9 @@ from undyingkingdoms import app, db
 @app.route("/reset_schema")
 def reset_schema():
     from flask_login import current_user
-    if current_user.name not in ['...']:
-        pass
+    # if current_user.name not in ['...']:
+    #     pass
         # redirect away from page
     db.drop_all()
     db.create_all()
-    return "Rebuilt database schema"
+    return "Rebuilt database schema!"
