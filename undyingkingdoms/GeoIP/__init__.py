0bc6d70ac70d566ff12674241bc05dbc5c650fd9
Marlen Brunner <klondikemarlen@gmail.com>
2018-12-27 19:32:59 -0800

    Update and rename GeoIP check IP access point.
    Add manual /update?force=true and /check?ip=... parameters.
    
    NOTE: this version of GeoLite2-City.mmdb is correct ... but the one my update code generates is not.

--- undyingkingdoms/GeoIP/__init__.py
+++ undyingkingdoms/GeoIP/__init__.py
@@ -15,18 +15,22 @@ def touch(path):
         os.utime(path, None)
 
 
-@geo_ip.route('/pull_ip')
-def pull_ip():
-    # import pdb;pdb.set_trace()
+@geo_ip.route('/check')
+def check_ip_against_geo_db():
+    """Check your IP address for location data.
+
+    Options: /check?ip=... (an ip address)
+    """
     geo_ip_db = 'GeoLite2-City/GeoLite2-City.mmdb'
     geo_ip_db_path = os.path.join(current_app.static_folder, geo_ip_db)
-    # optionally
-    info = {
-        "Remote IP": request.remote_addr,
-        "Access route": request.access_route
-    }
 
-    ip = request.access_route[0]
+    arg_ip = request.args.get('ip')
+    ip = arg_ip if arg_ip else request.access_route[0]
+
+    info = {
+        "Remote IP": ip,
+        "Access route": 'Request via /check?ip=...' if arg_ip else request.access_route
+    }
     with maxminddb.open_database(geo_ip_db_path) as reader:
         data = reader.get(ip)
         try:
@@ -41,18 +45,31 @@ def pull_ip():
 
 @geo_ip.route('/update')
 def update_geo_ip_database():
+    """Update the current IP geo-location database.
+
+    Options: /update?force=true (an ip address)
+
+    Note: I'm not sure if my code is bugged or if the current
+    downloadable database is.
+    """
     import urllib.request
     import shutil
     import tarfile
     from datetime import datetime
 
+    force = request.args.get('force') == 'true'
+
     geo_ip_db_folder = os.path.join(current_app.static_folder, 'GeoLite2-City')
 
     # creation file name format is 'create_YYYYMMDD'
-    date = [f for f in os.listdir(geo_ip_db_folder) if f.startswith('created')].pop()[-8:]
-    month = int(date[4:6])
+    try:
+        date = [f for f in os.listdir(geo_ip_db_folder) if f.startswith('created')].pop()[-8:]
+        month = int(date[4:6])
+    except FileNotFoundError:
+        # force update
+        month = datetime.today().month - 2
     # first check if current file is up to date
-    if ((datetime.today().month - month) % 12) > 1:
+    if force or ((datetime.today().month - month) % 12) > 1:
         # file is not up to date so download the new version
         base_name = 'GeoLite2-City.tar.gz'
         geo_url = 'https://geolite.maxmind.com/download/geoip/database/' + base_name
@@ -60,7 +77,7 @@ def update_geo_ip_database():
         try:
             with urllib.request.urlopen(geo_url) as f_in:
                 with open(tmp_tar_gz, 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
+                    f_out.write(f_in.read())
         except Exception as ex:
             return str(ex)
 
@@ -84,7 +101,7 @@ def update_geo_ip_database():
             shutil.copytree('/tmp/geo_ip/' + timestamped_folder, '/tmp/geo_ip/' + folder)
         except FileExistsError:
             pass
-        shutil.move('/tmp/geo_ip/' + folder, current_app.static_folder)
+        shutil.copytree('/tmp/geo_ip/' + folder, geo_ip_db_folder)
 
         # create timestamp file
         touch(geo_ip_db_folder + '/created_' + date_str)
