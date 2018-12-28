1597558921378955cdf28c3cb9c7891cf3d79fe2
Marlen Brunner <klondikemarlen@gmail.com>
2018-12-27 16:16:53 -0800

    Designed check code for when to update the geo-location database.

--- undyingkingdoms/GeoIP/__init__.py
+++ undyingkingdoms/GeoIP/__init__.py
@@ -1,34 +1,35 @@
 import os
 
 import maxminddb
-from flask import Blueprint, request, current_app
+from flask import Blueprint, request, current_app, url_for
 
 # causes import loop, need to refactor model design
 # from undyingkingdoms.models.users import User
 
 geo_ip = Blueprint('geoip', __name__)
 
+geo_ip_db = 'GeoLite2-City/GeoLite2-City.mmdb'
+
 
 @geo_ip.before_app_request
 def pull_ip_hook():
     # import pdb;pdb.set_trace()
-    city_reader = maxminddb.open_database(os.path.join(current_app.static_folder, 'GeoLite2-City_20181218', 'GeoLite2-City.mmdb'))
-    # optionally url_for('static' + 'GeoLite2-Country.mmdb')
+    geo_ip_db_path = os.path.join(current_app.static_folder, geo_ip_db)
+    # optionally
     current_app.logger.info("Remote IP: " + repr(request.remote_addr))
     current_app.logger.info("Access route: " + repr(request.access_route))
-    try:
-        ip = request.access_route[0]
-        # import pdb;pdb.set_trace()
-        data = city_reader.get(ip)
-        current_app.logger.info("Country: " + repr(data['country']['iso_code']))
-        current_app.logger.info("Subdivisions: " + repr([division['names']['en'] for division in data['subdivisions']]))
-        current_app.logger.info("City: " + repr(data['city']['names']['en']))
-        # Store in User object?
-    except TypeError:
-        current_app.logger.info("Location: Not Found")
-    finally:
-        city_reader.close()
-        return None  # Must return None or the request won't propagate.
+    ip = request.access_route[0]
+
+    with maxminddb.open_database(geo_ip_db_path) as reader:
+        data = reader.get(ip)
+        try:
+            current_app.logger.info("Country: " + repr(data['country']['iso_code']))
+            current_app.logger.info("Subdivisions: " + repr([division['names']['en'] for division in data['subdivisions']]))
+            current_app.logger.info("City: " + repr(data['city']['names']['en']))
+            # Store in User object?
+        except TypeError:
+            current_app.logger.info("Location: Not Found")
+    return None  # Must return None or the request won't propagate.
 
 
 @geo_ip.route('/geo_ip/update')
@@ -36,23 +37,29 @@ def update_geo_ip_database():
     import urllib.request
     import shutil
     import tarfile
+    from datetime import datetime, timedelta
 
+    geo_ip_db_folder = os.path.join(current_app.static_folder, 'GeoLite2-City')
     # first check if current file is up to date
+    # creation file name format is 'create_YYYYMMDD'
+    date = [f for f in os.listdir(geo_ip_db_folder) if f.startswith('created')].pop()[-8:]
+    month = int(date[4:6])
+    if True or ((datetime.today().month - month) % 12) > 1:
+        # if not download the new version
 
-    # if not download the new version
+        # if that succeeds delete the old one
 
-    # if that succeeds delete the old one
+        # replace with new one and update file paths
+        base_name = 'GeoLite2-City.tar.gz'
+        # geo_url = 'https://geolite.maxmind.com/download/geoip/database/' + base_name
+        tmp_tar_gz = '/tmp/' + base_name
+        # with urllib.request.urlopen(geo_url) as f_in:
+        #     with open(tmp_tar_gz, 'wb') as f_out:
+        #         shutil.copyfileobj(f_in, f_out)
 
-    # replace with new one and update file paths
-    base_name = 'GeoLite2-City.tar.gz'
-    geo_url = 'https://geolite.maxmind.com/download/geoip/database/' + base_name
-    tmp_tar_gz = '/tmp/' + base_name
-    with urllib.request.urlopen(geo_url) as f_in:
-        with open(tmp_tar_gz, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
+        with tarfile.open(tmp_tar_gz, "r:gz") as tar:
+            tar.extractall('/tmp/geo_ip/')
 
-    tar = tarfile.open(tmp_tar_gz, "r:gz")
-    tar.extractall('/tmp/')
-
-    return "Geo-location database has been updated."
+        return "Geo-location database has been updated."
+    return "Geo-location database is fairly current."
 
