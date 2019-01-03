47290bd0e7f787716902efe1337af0d078b6b395
elthran <jacobbrunner@gmail.com>
2019-01-03 14:28:33 -0800

    Added threads to forum again

--- undyingkingdoms/routes/user/forum.py
+++ undyingkingdoms/routes/user/forum.py
@@ -14,10 +14,12 @@ def forum(thread_id=0):
         
     the_forum = Forum.query.first()
     the_thread = Thread.query.filter_by(id=thread_id).first()
-    
-    form = ForumPost()
-    if form.validate_on_submit() and the_thread is not None:
-        new_post = Post(the_thread.id, current_user.id, form.title.data, form.message.data)
-        new_post.save()
-        return redirect(url_for('forum'))
+    if the_thread is None:
+        the_thread = "None"
+    else:
+        form = ForumPost()
+        if form.validate_on_submit() and the_thread is not None:
+            new_post = Post(the_thread.id, current_user.id, form.title.data, form.message.data)
+            new_post.save()
+            return redirect(url_for('forum'))
     return render_template('user/forum.html', form=form, forum=the_forum, the_thread=the_thread)
