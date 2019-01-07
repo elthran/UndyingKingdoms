60e8720791a7d13af40975109f0a97d091ab091f
elthran <jacobbrunner@gmail.com>
2019-01-06 20:26:21 -0800

    Minor UI tweaks

--- undyingkingdoms/routes/gameplay/overview.py
+++ undyingkingdoms/routes/gameplay/overview.py
@@ -5,7 +5,7 @@ from flask_login import login_required, current_user
 from sqlalchemy import desc
 
 from undyingkingdoms import app
-from undyingkingdoms.models import County, Kingdom, User, Infiltration
+from undyingkingdoms.models import World, County, Kingdom, User, Infiltration
 
 
 @app.route('/gameplay/overview/<int:kingdom_id>/<int:county_id>/', methods=['GET', 'POST'])
@@ -15,6 +15,8 @@ def overview(kingdom_id=0, county_id=0):
         time_since_last_activity = datetime.now() - user.time_modified
         if time_since_last_activity.total_seconds() > 300:  # A user who hasn't done anything in 5 minutes
             user.logged_in = False
+    world = World.query.first()
+    world.check_clock()
     if not current_user.logged_in:
         current_user.logged_in = True
     if not current_user.county:
