2ff1e0f3fb9bfdf7cfc4d6e2836afdea6c0f286c
Marlen Brunner <klondikemarlen@gmail.com>
2019-01-13 11:00:51 -0800

    Fix import path.

--- undyingkingdoms/scheduler/hourly.py
+++ undyingkingdoms/scheduler/hourly.py
@@ -1,6 +1,12 @@
-from undyingkingdoms.models import World
+import sys
+
+path = u'/home/undyingkingdoms/UndyingKingdoms/'
+if path not in sys.path:
+    sys.path = [path] + sys.path
+
+import undyingkingdoms.models as models
 
 # Advance the day
-world = World.query.first()
+world = models.World.query.first()
 world.advance_day()
 
