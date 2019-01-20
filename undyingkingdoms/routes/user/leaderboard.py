c20c9d601f1e4a6c01bdc36aaf07ad4f79446e24
elthran <jacobbrunner@gmail.com>
2019-01-19 16:55:00 -0800

    Hotfixes

--- undyingkingdoms/routes/user/leaderboard.py
+++ undyingkingdoms/routes/user/leaderboard.py
@@ -10,5 +10,6 @@ def leaderboard():
     if not current_user.in_active_session:
         current_user.in_active_session = True
     users = User.query.all()
-    sorted_users = sorted(users, key=lambda user: user.get_current_leaderboard_score(), reverse=True)
+    current_users = [user for user in users if user.county is not None]
+    sorted_users = sorted(current_users, key=lambda user: user.get_current_leaderboard_score(), reverse=True)
     return render_template('user/leaderboard.html', users=sorted_users)
