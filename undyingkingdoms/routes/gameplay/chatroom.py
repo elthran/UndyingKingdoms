5f15b437eeb16689c49e7aacefb39712ddc9593e
Marlen Brunner <klondikemarlen@gmail.com>
2019-01-20 12:40:28 -0800

    Fully functioned chat thingy.
    Mostly debugged new formatting issues created by migration.

--- undyingkingdoms/routes/gameplay/chatroom.py
+++ undyingkingdoms/routes/gameplay/chatroom.py
@@ -1,75 +1,39 @@
-from datetime import datetime
-
 from flask import render_template, jsonify, request
 from flask.views import MethodView
-from flask import render_template, url_for, redirect
 from flask_login import login_required, current_user
 
-from undyingkingdoms import app
 from undyingkingdoms.models import Chatroom
 from undyingkingdoms.models.forms.message import MessageForm
-
-
-@app.route('/gameplay/chatroom/', methods=['GET', 'POST'])
-@login_required
-def chatroom():
-    if not current_user.in_active_session:
-        current_user.in_active_session = True
-    form = MessageForm()
-    chat = Chatroom.query.filter_by(kingdom_id=current_user.county.kingdom_id).all()
-    if form.validate_on_submit():
-        message = Chatroom(current_user.county.kingdom_id, current_user.county.id, form.content.data)
-        message.save()
-        return redirect(url_for('chatroom'))
-    return render_template('gameplay/chatroom.html', form=form, chat=chat)
-# @app.route('/gameplay/chatroom/', methods=['GET', 'POST'])
-# @login_required
-# def chatroom():
-#     if not current_user.in_active_session:
-#         current_user.in_active_session = True
-#     chat_id = current_user.county.kingdom.id
-#     if current_user.county.kingdom.id not in global_chatroom:
-#         global_chatroom[chat_id] = []
-#     form = ChatForm()
-#     if form.validate_on_submit():
-#         global_chatroom[chat_id].append((str(datetime.now())[10:19], current_user.county.leader, form.message.data))
-#         global_chatroom[chat_id] = global_chatroom[chat_id][-20:]  # Only keeps most recent 20 items
-#         return redirect(url_for('chatroom'))
-#     return render_template('gameplay/chatroom.html', form=form, chat=global_chatroom[chat_id])
 
 
 class ChatRoomAPI(MethodView):
     @login_required
     def get(self):
-        app.logger.critical("Reached get method.")
         if not current_user.in_active_session:
             current_user.in_active_session = True
-        chat_id = current_user.county.kingdom.id
-        if current_user.county.kingdom.id not in global_chatroom:
-            global_chatroom[chat_id] = []
-        form = ChatForm()
-        return render_template('gameplay/chatroom.html', form=form, chat=global_chatroom[chat_id])
+        form = MessageForm()
+        chat = Chatroom.query.filter_by(kingdom_id=current_user.county.kingdom_id).all()
+        return render_template('gameplay/chatroom.html', form=form, chat=chat)
 
     @login_required
     def post(self):
         if not current_user.in_active_session:
             current_user.in_active_session = True
-        chat_id = current_user.county.kingdom.id
-        form = ChatForm()
+        form = MessageForm()
+        chat = Chatroom.query.filter_by(kingdom_id=current_user.county.kingdom_id).all()
         if request.form['updateOnly'] == 'true':
             return jsonify(dict(
                 status='success',
                 message='Here is the latest data.',
-                data=global_chatroom,
+                data=(message.json_ready() for message in chat)
             ))
         elif form.validate_on_submit():
-            entry = (str(datetime.now())[10:19], current_user.county.leader, form.message.data)
-            global_chatroom[chat_id].append(entry)
-            global_chatroom[chat_id] = global_chatroom[chat_id][-20:]  # Only keeps most recent 20 items
+            message = Chatroom(current_user.county.kingdom_id, current_user.county.id, form.content.data)
+            message.save()
             return jsonify(dict(
                 status='success',
-                message='Here is your chat data.',
-                data=entry,
+                message='Here is your message.',
+                data=message.json_ready(),
             ))
         return jsonify(dict(
             status='fail',
