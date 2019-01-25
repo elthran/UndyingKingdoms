c7e8a11ff1e381126c9cecd57fa2232a4292750e
elthran <jacobbrunner@gmail.com>
2019-01-24 21:10:19 -0800

    Fixed inbos bug

--- undyingkingdoms/routes/gameplay/messages.py
+++ undyingkingdoms/routes/gameplay/messages.py
@@ -2,6 +2,7 @@ from flask import render_template
 from flask_login import login_required, current_user
 
 from undyingkingdoms import app
+from undyingkingdoms.models import Message
 
 
 @app.route('/gameplay/messages/', methods=['GET', 'POST'])
@@ -9,4 +10,7 @@ from undyingkingdoms import app
 def messages():
     if not current_user.in_active_session:
         current_user.in_active_session = True
+    new_messages = Message.query.filter_by(county_id=current_user.county.id, unread=True).all()
+    for message in new_messages:
+        message.unread = False
     return render_template('gameplay/messages.html')
