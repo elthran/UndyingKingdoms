e22157e4b75ea38c8d0ffd998e3895bbe65aa371
elthran <jacobbrunner@gmail.com>
2019-01-25 09:51:13 -0800

    Thieving now generates a notice for the targetted player

--- undyingkingdoms/routes/gameplay/infiltrate.py
+++ undyingkingdoms/routes/gameplay/infiltrate.py
@@ -4,7 +4,7 @@ from flask import redirect, url_for, render_template
 from flask_login import login_required, current_user
 
 from undyingkingdoms import app
-from undyingkingdoms.models import Infiltration, County
+from undyingkingdoms.models import Infiltration, County, Notification
 from undyingkingdoms.models.forms.infiltrate import InfiltrateForm
 from undyingkingdoms.static.metadata import infiltration_missions, all_buildings
 
@@ -43,20 +43,38 @@ def infiltrate(county_id):
                 current_user.county.gold += gold_stolen
                 report.pilfer_amount = gold_stolen
                 report.duration = 14
+                notification = Notification(target.id,
+                                            "Thieves raided our lands",
+                                            "They stole {} gold from our coffers".format(gold_stolen),
+                                            current_user.county.kingdom.world.day)
             elif mission == 'burn crops':
                 crops_burned = min(target.buildings['fields'].total, form.amount.data)
                 target.buildings['fields'].total -= crops_burned
                 report.crops_burned = crops_burned
+                notification = Notification(target.id,
+                                            "Thieves raided our lands",
+                                            "They burned {} of our crops".format(crops_burned),
+                                            current_user.county.kingdom.world.day)
             elif mission == 'sow distrust':
                 happiness_lost = min(target.happiness, form.amount.data * 3)
                 target.happiness -= happiness_lost
                 report.distrust = happiness_lost
                 report.duration = 18
+                notification = Notification(target.id,
+                                            "Thieves raided our lands",
+                                            "They have caused some unrest in our kingdom",
+                                            current_user.county.kingdom.world.day)
             elif mission == 'scout military':
                 report.get_troop_report(current_user.county, target, form.amount.data)
                 report.duration = 6
+                notification = Notification(target.id,
+                                            "Thieves raided our lands",
+                                            "They have found out some secrets regarding our military",
+                                            current_user.county.kingdom.world.day)
         else:
+            notification = Notification(target.id, "You caught enemy thieves from {}".format(current_user.county.name), "You caught them before they could accomplish their task", current_user.county.kingdom.world.day)
             report.success = False
+        notification.save()
 
         return redirect(url_for('infiltration'))
 
