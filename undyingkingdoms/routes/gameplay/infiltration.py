76f39250b72eaf730198a0db7cc425954ab76db8
Marlen T. B <klondikemarlen@gmail.com>
2019-01-30 11:54:41 -0800

    Infiltration2mobile (#182)
    * Begin mobile versions of infiltrate/infiltration pages.
    
    * Finalize new look for infiltration page.
    
    Debug broken code from origin/master merge.
    
    * Finalize new look for infiltrate page.
    
    Add :hover :active tweaks to button in main css.

--- undyingkingdoms/routes/gameplay/infiltration.py
+++ undyingkingdoms/routes/gameplay/infiltration.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask_login import login_required, current_user
+from flask_mobility.decorators import mobile_template
 from sqlalchemy import desc
 
 from undyingkingdoms import app
@@ -7,9 +8,10 @@ from undyingkingdoms.models import Infiltration
 
 
 @app.route('/gameplay/infiltration/', methods=['GET', 'POST'])
+@mobile_template("{mobile/}gameplay/infiltration.html")
 @login_required
-def infiltration():
+def infiltration(template):
     if not current_user.in_active_session:
         current_user.in_active_session = True
     missions = Infiltration.query.filter_by(county_id=current_user.county.id).order_by(desc('time_created')).all()
-    return render_template('gameplay/infiltration.html', missions=missions)
+    return render_template(template, missions=missions)
