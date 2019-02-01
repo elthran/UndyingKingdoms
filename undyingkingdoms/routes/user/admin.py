4bd03f85e6ffb3da729e4e3397561dd9023494e5
elthran <jacobbrunner@gmail.com>
2019-01-31 19:42:03 -0800

    Fixed bug with empty kingdom not getting populated

--- undyingkingdoms/routes/user/admin.py
+++ undyingkingdoms/routes/user/admin.py
@@ -13,7 +13,7 @@ from undyingkingdoms.models.users import User
 
 
 class AdminForm(FlaskForm):
-	submit = SubmitField('Do this')
+    submit = SubmitField('Do this')
 
 
 bot_county_prefix = ["Eldritch", "Dengmar", "Zort", "Pralde", "Eggroth", "Wuanlo"]
@@ -26,21 +26,24 @@ bot_leader_suffix = ["oth", "ith", "ion", "ilisk", "anth", "ills", "ondo", "alas
 @login_required
 @app.route('/user/admin/', methods=['GET', 'POST'])
 def admin():
-	if not current_user.in_active_session:
-		current_user.in_active_session = True
-	bot_form = AdminForm()
-	kingdoms = Kingdom.query.all()
-	smallest_kingdom = min(kingdoms, key=lambda x: len(x.counties))
-	if bot_form.validate_on_submit():
-		for i in range(3):
-			bot_name = uuid4()
-			user = User("bot_{}".format(bot_name), "bot_{}@gmail.com".format(bot_name), "1234")
-			user.is_bot = True
-			user.save()
-			county = County(smallest_kingdom.id, "{}{}".format(choice(bot_county_prefix), choice(bot_county_suffix)),
+    if not current_user.in_active_session:
+        current_user.in_active_session = True
+    bot_form = AdminForm()
+    kingdoms = Kingdom.query.all()
+    if bot_form.validate_on_submit():
+        for i in range(3):
+            smallest_kingdom = min(kingdoms, key=lambda x: len(x.counties))
+            for kingdom in kingdoms:
+                if kingdom.counties is None:
+                    smallest_kingdom = kingdom
+            bot_name = uuid4()
+            user = User("bot_{}".format(bot_name), "bot_{}@gmail.com".format(bot_name), "1234")
+            user.is_bot = True
+            user.save()
+            county = County(smallest_kingdom.id, "{}{}".format(choice(bot_county_prefix), choice(bot_county_suffix)),
                             "{}{}".format(choice(bot_leader_prefix), choice(bot_leader_suffix)), user.id,
-			                choice(["Human", "Elf", "Dwarf"]), choice(["Male", "Female"]),
-			                choice(["Engineer", "Warlord", "Rogue", "Merchant"]))
-			county.save()
-			county.vote = county.id
-	return render_template('user/admin.html', form=bot_form)
+                            choice(["Human", "Elf", "Dwarf"]), choice(["Male", "Female"]),
+                            choice(["Engineer", "Warlord", "Rogue", "Merchant"]))
+            county.save()
+            county.vote = county.id
+    return render_template('user/admin.html', form=bot_form)
