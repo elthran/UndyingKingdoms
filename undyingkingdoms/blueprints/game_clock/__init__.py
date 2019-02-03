e5bbb6a6d7c33c7d0badfdb066a46a2e4b340250
Marlen Brunner <klondikemarlen@gmail.com>
2019-02-03 15:42:41 -0800

    Begin working on overview_enemy.html to mobile view.
    Migrated closing of inactive sessions to a scheduled task file that should run every 15 minutes.

--- undyingkingdoms/blueprints/game_clock/__init__.py
+++ undyingkingdoms/blueprints/game_clock/__init__.py
@@ -4,6 +4,7 @@ from .views.advance_day import AdvanceDayAPI
 from .views.token import TokenAPI
 from .views.advance_24h_analytics import Advance24hAnalyticsAPI
 from .views.advance_age import AdvanceAgeAPI
+from .views.close_inactive_sessions import CloseInactiveSessionsAPI
 
 game_clock_blueprint = Blueprint('game_clock', __name__, url_prefix='/game_clock')
 
@@ -11,3 +12,4 @@ game_clock_blueprint.add_url_rule('/advance_day', view_func=AdvanceDayAPI.as_vie
 game_clock_blueprint.add_url_rule('/token', view_func=TokenAPI.as_view('token_api'))
 game_clock_blueprint.add_url_rule('/advance_24h_analytics', view_func=Advance24hAnalyticsAPI.as_view('advance_24h_analytics_api'))
 game_clock_blueprint.add_url_rule('/advance_age', view_func=AdvanceAgeAPI.as_view('advance_age_api'))
+game_clock_blueprint.add_url_rule('/close_inactive_sessions', view_func=CloseInactiveSessionsAPI.as_view('close_inactive_sessions_api'))
