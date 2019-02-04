ed26efe8c972f0df73abf51473b7de8c552b5b62
Marlen T. B <klondikemarlen@gmail.com>
2019-02-03 16:20:47 -0800

    Provide a mobile view for inbox/message route. (#193)
    * Update message html to provide a mobile view.
    
    * Add in new mobile message.html that I missed last time.
    
    * Begin working on overview_enemy.html to mobile view.
    
    Migrated closing of inactive sessions to a scheduled task file that should run every 15 minutes.
    
    * Move {% set %} code inside of {% block %} tags -> prevent mangling.
    
    * Finalize look for enemy overview mobile page.
    
    I'm using some CSS3 code (calc) which will hopefully be ok.

--- undyingkingdoms/routes/gameplay/messages.py
+++ undyingkingdoms/routes/gameplay/messages.py
@@ -1,16 +1,18 @@
 from flask import render_template
 from flask_login import login_required, current_user
+from flask_mobility.decorators import mobile_template
 
 from undyingkingdoms import app
 from undyingkingdoms.models import Message
 
 
 @app.route('/gameplay/messages/', methods=['GET', 'POST'])
+@mobile_template('{mobile/}gameplay/messages.html')
 @login_required
-def messages():
+def messages(template):
     if not current_user.in_active_session:
         current_user.in_active_session = True
     new_messages = Message.query.filter_by(county_id=current_user.county.id, unread=True).all()
     for message in new_messages:
         message.unread = False
-    return render_template('gameplay/messages.html')
+    return render_template(template)
--- undyingkingdoms/routes/gameplay/overview.py
+++ undyingkingdoms/routes/gameplay/overview.py
@@ -5,30 +5,23 @@ from flask_login import login_required, current_user
 from flask_mobility.decorators import mobile_template
 
 from undyingkingdoms import app
-from undyingkingdoms.models import County, Kingdom, User, Message
+from undyingkingdoms.models import County, Kingdom, Message
 from undyingkingdoms.models.forms.message import MessageForm
+from undyingkingdoms.routes.helpers import in_active_session
 
 
 @app.route('/gameplay/overview/<int:kingdom_id>/<int:county_id>/', methods=['GET', 'POST'])
 @mobile_template('{mobile/}gameplay/overview.html')
 @login_required
+@in_active_session
 def overview(template, kingdom_id=0, county_id=0):
-    for user in User.query.filter_by(_in_active_session=True).all():
-        time_since_last_activity = datetime.utcnow() - user.time_modified
-        if time_since_last_activity.total_seconds() > 300:  # A user who hasn't done anything in 5 minutes
-            user.in_active_session = False
-    if not current_user.in_active_session:
-        current_user.in_active_session = True
     if not current_user.county:
         return redirect(url_for('initialize'))
-    if kingdom_id == current_user.county.kingdom_id and county_id == current_user.county.id:
-        kingdom_id = 0
-        county_id = 0
-
-    if kingdom_id == 0 or county_id == 0:
+    if (kingdom_id == current_user.county.kingdom_id and county_id == current_user.county.id) or (kingdom_id == 0 or county_id == 0):
         has_mail = Message.query.filter_by(county_id=current_user.county.id, unread=True).count()
         return render_template(template, has_mail=has_mail)
 
+    # Would like to move to a POST view.
     form = MessageForm()
     if form.validate_on_submit():
         message = Message(county_id=county_id,
@@ -41,5 +34,9 @@ def overview(template, kingdom_id=0, county_id=0):
 
     target_kingdom = Kingdom.query.filter_by(id=kingdom_id).first()
     target_county = County.query.filter_by(id=county_id).first()
-    # todo: work out how to represent this template for mobile.
-    return render_template('gameplay/overview_enemy.html', target_kingdom=target_kingdom, target_county=target_county, form=form)
+
+    # Would like to move to a view_enemy route of some kind.
+    # Ugly hack to return '{mobile/}gameplay/overview_enemy.html'
+    template = '_enemy.'.join(template.split('.'))
+    print(template)
+    return render_template(template, target_kingdom=target_kingdom, target_county=target_county, form=form)
