0c6062b39bdc28960efd5451fc83762913a1d49a
Marlen Brunner <klondikemarlen@gmail.com>
2019-02-04 19:51:11 -0800

    Set hard limits on character size for message input fields.

--- undyingkingdoms/models/forms/message.py
+++ undyingkingdoms/models/forms/message.py
@@ -2,10 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Length
 
-
 class MessageForm(FlaskForm):
-    title = StringField('Title',
-                        validators=[Length(min=0, max=32)])
-    content = StringField('Content',
-                          validators=[DataRequired(message='Your post requires a message.'), Length(min=0, max=5000)])
+    TITLE_SIZE = 32
+    CONTENT_SIZE = 5000
+    title = StringField('Title', validators=[Length(min=0, max=TITLE_SIZE)])
+    content = StringField('Content', validators=[DataRequired(message='Your post requires a message.'), Length(min=0, max=CONTENT_SIZE)])
 
