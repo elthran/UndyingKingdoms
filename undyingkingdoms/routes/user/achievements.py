87365792cb98ec4252866e374c0b9ba7c66297a0
Marlen Brunner <klondikemarlen@gmail.com>
2019-02-05 17:03:30 -0800

    Migrate achievements page to mobile.
    Begin building a global mobile styles sheet.

--- undyingkingdoms/routes/user/achievements.py
+++ undyingkingdoms/routes/user/achievements.py
@@ -1,14 +1,16 @@
 from flask import render_template, redirect
 from flask_login import login_required, current_user
+from flask_mobility.decorators import mobile_template
 
 from undyingkingdoms import app, db
 from undyingkingdoms.models import Achievement
+from undyingkingdoms.routes.helpers import in_active_session
 
 
-@login_required
 @app.route('/user/achievements/', methods=['GET', 'POST'])
-def achievements():
-    if not current_user.in_active_session:
-        current_user.in_active_session = True
+@mobile_template('{mobile/}user/achievements.html')
+@login_required
+@in_active_session
+def achievements(template):
     all_achievements = Achievement.query.filter_by(user_id=current_user.id).all()
-    return render_template('user/achievements.html', achievements=all_achievements)
+    return render_template(template, achievements=all_achievements)
