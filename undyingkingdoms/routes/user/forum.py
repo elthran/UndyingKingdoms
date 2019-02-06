3168a64ef093a858839f76bd0a2ef95bf8c57a93
Marlen T. B <klondikemarlen@gmail.com>
2019-02-05 19:56:21 -0800

    Forum2mobile (#213)
    * Migrate forum to mobile edition.
    
    I'm not that happy with it honestly, I wanted to cut the file into 3 parts but couldn't quite work out how.
    
    * Add file I missed because my selections go away when I exit this.

--- undyingkingdoms/routes/user/forum.py
+++ undyingkingdoms/routes/user/forum.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for
 from flask_login import login_required, current_user
+from flask_mobility.decorators import mobile_template
 
 from undyingkingdoms import app
 from undyingkingdoms.models.forms.message import MessageForm
@@ -8,9 +9,10 @@ from undyingkingdoms.routes.helpers import in_active_session
 
 
 @app.route('/user/forum/<int:thread_id>/<int:post_id>', methods=['GET', 'POST'])
+@mobile_template('{mobile/}user/forum.html')
 @login_required
 @in_active_session
-def forum(thread_id=0, post_id=0):
+def forum(template, thread_id=0, post_id=0):
     the_forum = Forum.query.first()
     the_thread = Thread.query.filter_by(id=thread_id).first()
     the_post = Post.query.filter_by(id=post_id).first()
@@ -25,4 +27,4 @@ def forum(thread_id=0, post_id=0):
         if post_id == 0:
             post_id = new_post.id  #
         return redirect(url_for('forum', thread_id=thread_id, post_id=post_id))
-    return render_template('user/forum.html', form=form, forum=the_forum, the_thread=the_thread, the_post=the_post)
+    return render_template(template, form=form, forum=the_forum, the_thread=the_thread, the_post=the_post)
