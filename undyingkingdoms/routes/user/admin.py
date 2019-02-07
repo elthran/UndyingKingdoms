e1cd70057d5cc74b11dfff0be76f972070bb342a
elthran <jacobbrunner@gmail.com>
2019-02-07 11:22:55 -0800

    Added more bot names to reduce repeats

--- undyingkingdoms/routes/user/admin.py
+++ undyingkingdoms/routes/user/admin.py
@@ -18,8 +18,8 @@ class AdminForm(FlaskForm):
     submit = SubmitField('Do this')
 
 
-bot_county_prefix = ["Eldritch", "Dengmar", "Zort", "Pralde", "Eggroth", "Wuanlo"]
-bot_county_suffix = ["ville", " Town", "ion", "oth", " Land"]
+bot_county_prefix = ["Eldritch", "Dengmar", "Zort", "Pralde", "Eggroth", "Wuanlo", "Ildar", "Endo", "Vil", "Gong", "Fili"]
+bot_county_suffix = ["ville", " Town", "ion", "oth", " Land", "don", "min", " Vale"]
 
 bot_leader_prefix = ["Skri", "Aldar", "Frel", "Eldr", "Toth", "Zon", "Pandish", "Gresk", "Linsk"]
 bot_leader_suffix = ["oth", "ith", "ion", "ilisk", "anth", "ills", "ondo", "alasl"]
