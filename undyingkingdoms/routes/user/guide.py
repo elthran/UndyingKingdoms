92e3e5ef1fa7ef859088b942ccd7730e4187b74f
Marlen T. B <klondikemarlen@gmail.com>
2019-02-09 14:01:43 -0800

    Fix 194 - add a page to view all unit stats. (#237)
    * Slowly tinkering up to generating a race comparison table.
    
    * Fix peasant class name.
    
    * Trying to extract army date from it's dataframe.
    
    I can get the data in, but not out :P
    
    * Mostly working Race Guide data.
    
    * Add generated comparison divs for all races and classes.
    
    * Fix formatting of mobile compare table.
    
    Rename New Player Guide, to Player Guide.

--- undyingkingdoms/routes/user/guide.py
+++ undyingkingdoms/routes/user/guide.py
@@ -1,5 +1,7 @@
+import pandas as pd
+
 from flask import render_template
-from flask_login import login_required, current_user
+from flask_login import login_required
 from flask_mobility.decorators import mobile_template
 
 from undyingkingdoms import app
@@ -11,4 +13,8 @@ from undyingkingdoms.routes.helpers import in_active_session
 @login_required
 @in_active_session
 def guide(template):
-    return render_template(template)
+
+    # these should get generated here too ...
+    races = pd.read_excel('undyingkingdoms/static/metadata/modifiers.xlsx', sheet_name='Modifiers', index_col=0)
+    armies = pd.read_excel('undyingkingdoms/static/metadata/armies.xlsx', sheet_name='Armies', index_col=0)
+    return render_template(template, races=races, armies=armies)
