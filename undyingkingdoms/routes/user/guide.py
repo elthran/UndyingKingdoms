a463f6c74166b2b18a018e8cbffa7beb4a198c14
Marlen Brunner <klondikemarlen@gmail.com>
2019-02-09 13:31:56 -0800

    Add generated comparison divs for all races and classes.

--- undyingkingdoms/routes/user/guide.py
+++ undyingkingdoms/routes/user/guide.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
 from flask import render_template
-from flask_login import login_required, current_user
+from flask_login import login_required
 from flask_mobility.decorators import mobile_template
 
 from undyingkingdoms import app
-from undyingkingdoms.static import metadata
 from undyingkingdoms.routes.helpers import in_active_session
 
 
