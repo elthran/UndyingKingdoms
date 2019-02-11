c76c36ded28557366bd63dd7a77aec9120c0083a
Marlen Brunner <klondikemarlen@gmail.com>
2019-02-10 19:00:21 -0800

    Attempting to debug new county trade option.
    Very aggressive rework of overview route.

--- undyingkingdoms/models/forms/trade.py
+++ undyingkingdoms/models/forms/trade.py
@@ -7,7 +7,7 @@ class TradeForm(FlaskForm):
     offer_wood = SelectField('offer_wood', coerce=int)
     offer_iron = SelectField('offer_iron', coerce=int)
 
-    receive_gold = SelectField('receive_wood', coerce=int)
+    receive_gold = SelectField('receive_gold', coerce=int)
     receive_wood = SelectField('receive_wood', coerce=int)
     receive_iron = SelectField('receive_iron', coerce=int)
 
--- undyingkingdoms/routes/gameplay/overview.py
+++ undyingkingdoms/routes/gameplay/overview.py
@@ -1,47 +1,99 @@
 from datetime import datetime
 
-from flask import render_template, url_for, redirect, request
+from flask import render_template, url_for, redirect, request, jsonify
 from flask_login import login_required, current_user
 from flask_mobility.decorators import mobile_template
 
 from undyingkingdoms import app
 from undyingkingdoms.models import County, Kingdom, Message
+from undyingkingdoms.models.bases import GameState
 from undyingkingdoms.models.forms.message import MessageForm
 from undyingkingdoms.models.forms.trade import TradeForm
 from undyingkingdoms.routes.helpers import in_active_session
 
+"""
+Each of these routes could go in their own file
 
-@app.route('/gameplay/overview/<int:kingdom_id>/<int:county_id>/', methods=['GET', 'POST'])
+The main concept is that you are separating each concept into
+its own route. Even though this cause duplication of code it is much
+easeir to test and debug. If you use a major concept in 2 places
+you can make that a 'utility' function and import it into both routes.
+"""
+
+@app.route('/gameplay/overview', methods=['GET'])
 @mobile_template('{mobile/}gameplay/overview.html')
 @login_required
 @in_active_session
-def overview(template, kingdom_id=0, county_id=0):
+def overview(template):
+    # If game has been reset allow user to make a new county.
     if not current_user.county:
         return redirect(url_for('initialize'))
-    if (kingdom_id == current_user.county.kingdom_id and county_id == current_user.county.id) or (kingdom_id == 0 or county_id == 0):
-        has_mail = Message.query.filter_by(county_id=current_user.county.id, unread=True).count()
-        return render_template(template, has_mail=has_mail)
 
-    # Would like to move to a POST view.
+    has_mail = Message.query.filter_by(county_id=current_user.county.id, unread=True).count()
+    return render_template(template, has_mail=has_mail)
+
+
+@app.route('/gameplay/enemy_overview/<int:kingdom_id>/<int:county_id>/', methods=['GET'])
+@mobile_template('{mobile/}gameplay/enemy_overview.html')
+@login_required
+@in_active_session
+def enemy_overview(template, kingdom_id=0, county_id=0):
     message_form = MessageForm()
-    if request.args.get('id') == 'message' and message_form.validate_on_submit():
-        message = Message(county_id=county_id,
-                          title=message_form.title.data,
-                          content=message_form.content.data,
-                          author_county_id=current_user.county.id,
-                          day=current_user.county.kingdom.world.day)
-        message.save()
-        return redirect(url_for('overview', kingdom_id=kingdom_id, county_id=county_id))
-
     trade_form = TradeForm()
-    if request.args.get('id') == 'trade' and trade_form.validate_on_submit():
-        print("Trading:", trade_form)
-        return redirect(url_for('overview', kingdom_id=kingdom_id, county_id=county_id))
-
     target_kingdom = Kingdom.query.filter_by(id=kingdom_id).first()
     target_county = County.query.filter_by(id=county_id).first()
+    # import pdb;pdb.set_trace()
 
-    # Would like to move to a view_enemy route of some kind.
-    # Ugly hack to return '{mobile/}gameplay/overview_enemy.html'
-    template = '_enemy.'.join(template.split('.'))
     return render_template(template, target_kingdom=target_kingdom, target_county=target_county, message_form=message_form, trade_form=trade_form)
+
+
+@app.route('/gameplay/send_message/<int:county_id>/', methods=['POST'])
+@login_required
+@in_active_session
+def send_message(county_id):
+    message_form = MessageForm()
+    if message_form.validate_on_submit():
+        message = Message(
+            county_id=county_id,
+            title=message_form.title.data,
+            content=message_form.content.data,
+            author_county_id=current_user.county.id,
+            day=current_user.county.kingdom.world.day)
+        message.save()
+        return jsonify(dict(
+            status='success',
+            message=f'You sent a message to {county_id}'
+        ))
+    return jsonify(dict(
+        status='fail',
+        message="Your message didn't pass form validation."
+    ))
+
+# move to proper model file
+class Trade(GameState):
+    def __init__(self, kingdom_id, county_id):
+        pass
+
+
+@app.route('/gameplay/trade/<int:county_id>/', methods=['POST'])
+@login_required
+@in_active_session
+def trade(county_id):
+
+    county = County.query.get(county_id)
+    kingdom_id = county.kingdom_id
+
+    trade_form = TradeForm()
+    if trade_form.validate_on_submit():
+        trade = Trade(kingdom_id, county_id)
+
+        trade.save()
+        return jsonify(dict(
+            status='success',
+            message=f'You sent a trade to {county_id}'
+        ))
+    return jsonify(dict(
+        status='fail',
+        message="Your trade didn't pass form validation."
+    ))
+
