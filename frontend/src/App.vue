ce80ddfcb99fe63c2c289db3fea6367ef297b757
Marlen Brunner <klondikemarlen@gmail.com>
2019-02-12 15:23:38 -0800

    Managed to get some actual content to render in VueJS.

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <vue-title title="Economy"></vue-title>
+    <SideBar>
+      <p>Actual content</p>
+    </SideBar>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue"
+import SideBar from "./components/SideBar.vue"
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+    SideBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,77 @@
+<style type="text/css">
+#layout-body {
+    display: flex;
+    margin: 1em;
+}
+
+#sidebar {  /* The main sidebar which is always visible when logged in */
+    margin-right: 1em;
+    padding: 0.6em;
+    font-size: 1.25em;
+    line-height: 1.7em;
+    border: solid;
+    border-radius: 5px;
+}
+
+#layout-content {
+    width: 100%;
+}
+
+.notice {
+    font-weight:bold;
+}
+</style>
+
+<template>
+  <div id="layout-body">
+    <div id="sidebar">
+      <ul style="text-align:center;">
+        <li><a :href="url_for('overview', kingdom_id=0, county_id=0)">County&nbsp;Overview</a></li>
+        <li><a :href="url_for('economy')">Economy</a></li>
+        <li><a :href="url_for('infrastructure')">Infrastructure</a></li>
+        <li><a :href="url_for('military')">Military</a></li>
+        <li><a :href="url_for('infiltration')">Infiltration</a></li>
+        <li><a :href="url_for('messages')" v-bind:class="{ notice: hasMail }">Inbox</a></li>
+        <li><a :href="url_for('chatroom_api')">Town&nbsp;Hall</a></li>
+        <li><a :href="url_for('kingdom', kingdom_id=kingdomId)">Kingdom&nbsp;Overview</a></li>
+        <li><br></li>
+        <li><h1>About the Game</h1></li>
+        <li><a :href="url_for('achievements')">Achievements</a></li>
+        <li><a :href="url_for('forum', thread_id=0, post_id=0)">Forum</a></li>
+        <li><a :href="url_for('guide')">Player&nbsp;Guide</a></li>
+        <li><a :href="url_for('leaderboard')">Leaderboard</a></li>
+        <li><a :href="url_for('versions')">Read&nbsp;About&nbsp;Updates</a></li>
+        {% if user.is_admin %}<li><a :href="url_for('admin.home_api')">Admin</a></li>{% endif %}
+        <li><a :href="url_for('logout')">Logout</a></li>
+        <li><br><br><br></li>
+      </ul>
+    </div>
+    <div id="layout-content">
+      {% block content2 %}
+      {% endblock %}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  props: {
+    hasMail: Boolean,
+    content: String,
+    kingdomId: Number
+  /*
+  has_mail
+  {% set user = current_user %}
+{% set county = current_user.county %}
+{% set kingdom = county.kingdom %}
+is_admin
+  */
+  },
+  methods: {
+    url_for: function () {
+      console.log(arguments);
+    }
+  }
+}
+</script>
