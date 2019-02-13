2fc5b782f2ef3d86f04e88e5f5ef01be63497814
Marlen Brunner <klondikemarlen@gmail.com>
2019-02-12 17:05:14 -0800

    Finally made backend talk to frontend.

--- undyingkingdoms/routes/gameplay/economy.py
+++ undyingkingdoms/routes/gameplay/economy.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from flask_mobility.decorators import mobile_template
 
@@ -9,7 +9,7 @@ from undyingkingdoms.static.metadata.metadata import rations_terminology, birth_
     food_consumed_modifier, happiness_modifier
 
 
-@app.route('/gameplay/economy/', methods=['GET', 'POST'])
+@app.route('/gameplay/economy/', methods=['GET'])
 @mobile_template('{mobile/}gameplay/economy.html')
 @login_required
 @in_active_session
@@ -17,12 +17,32 @@ def economy(template):
     form = EconomyForm(tax=current_user.county.tax, rations=current_user.county.rations)
     form.tax.choices = [(i, i) for i in range(11)]
     form.rations.choices = [(pairing[0], pairing[1]) for pairing in rations_terminology]
-    if form.validate_on_submit():
-        current_user.county.tax = form.tax.data
-        current_user.county.rations = form.rations.data
-        return redirect(url_for('economy'))
     return render_template(template, form=form,
                            birth_rate_modifier=birth_rate_modifier,
                            income_modifier=income_modifier,
                            food_consumed_modifier=food_consumed_modifier,
                            happiness_modifier=happiness_modifier)
+
+
+@app.route('/gameplay/economy/update', methods=['POST'])
+@login_required
+@in_active_session
+def update_economy():
+    form = EconomyForm(tax=current_user.county.tax, rations=current_user.county.rations)
+    form.tax.choices = [(i, i) for i in range(11)]
+    form.rations.choices = [(pairing[0], pairing[1]) for pairing in rations_terminology]
+    if form.validate_on_submit():
+        current_user.county.tax = form.tax.data
+        current_user.county.rations = form.rations.data
+        return jsonify(dict(
+            status="success",
+            message="You have updated your economy data.",
+            birth_rate_modifier=birth_rate_modifier,
+            income_modifier=income_modifier,
+            food_consumed_modifier=food_consumed_modifier,
+            happiness_modifier=happiness_modifier
+        ))
+    return jsonify(dict(
+        status="fail",
+        message="You economy data did not pass form validation."
+    ))
