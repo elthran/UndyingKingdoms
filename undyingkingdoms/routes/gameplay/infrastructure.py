e10d9b4eda5476e29e7d944657e2e3cc462579c3
Marlen Brunner <klondikemarlen@gmail.com>
2019-02-14 15:37:29 -0800

    Bugfix sliders on desktop.

--- undyingkingdoms/routes/gameplay/infrastructure.py
+++ undyingkingdoms/routes/gameplay/infrastructure.py
@@ -12,6 +12,9 @@ def max_buildable_by_cost(county, building):
     max_size = min(
         county.gold // building.gold_cost,
         county.wood // building.wood_cost,
+        (county.stone // building.stone_cost
+            if building.stone_cost
+            else float('inf')),
         # eventually the 1 will be a land cost.
         county.get_available_land () // 1
     )
