fc620717623f3da1b26a89719e8544214f887438
Marlen Brunner <klondikemarlen@gmail.com>
2019-02-17 12:10:49 -0800

    Debug devLogin so it now runs with async: false.
    Now I should be able to test things in a more normal fashion.
    Hmmm ... I guess I have to work out the static assets thing, but I am getting there.

--- frontend/src/assets/devHelpers.js
+++ frontend/src/assets/devHelpers.js
@@ -10,54 +10,63 @@ function baseURL () {
   }
 }
 
-// $.ajax({
-//     async: false,
-//     type: "POST",
-//     url: "Default.aspx/GetCurrentTime",
-//     data: '{name: "Mudassar" }',
-//     contentType: "application/json; charset=utf-8",
-//     dataType: "json",
-//     success: function (response) {
-//         alert(response.d);
-//     }
-// });
-
 function login () {
   var loginUrl = 'http://localhost:5000/login/'
   var csrfToken;
 
-  axios.get(loginUrl, {withCredentials: true})
-  .then( (response, status) => {
-    console.log('GET login successful');
-    // console.log(response);
-    csrfToken = $('#csrf_token', response.data);
-    // console.log(csrfToken.val());
-    if (csrfToken.val() === undefined) {
-      console.log("You are already logged in!")
-    } else {
-      axios.post(loginUrl, 
-        $.param({
-          email: "haldon@gmail.com",
-          password: "brunner"
-        }), {
-        headers: {"X-CSRF-TOKEN": csrfToken.val()},
-        withCredentials: true
-      })
-      .then(function (response, status) {
-        console.log("POST login successful")
-        // console.log(response);
-      }).catch( (error) => {
-        console.log('POST login failed');
-        console.log(error);
-      });
+  $.ajax({
+    async: false,
+    type: "GET",
+    url: loginUrl,
+    xhrFields: {
+      withCredentials: true
+    },
+    success: (response, status) => {
+      // console.log('GET login successful');
+      // console.log(response);
+      csrfToken = $('#csrf_token', response);
+      // console.log(csrfToken.val());
+      if (csrfToken.val() === undefined) {
+        // console.log("You are already logged in!")
+      } else {
+        $.ajax({
+          async: false,
+          type: "POST",
+          url: loginUrl, 
+          data: $.param({
+            email: "haldon@gmail.com",
+            password: "brunner"
+          }), 
+          xhrFields: {
+            withCredentials: true
+          },
+          headers: {"X-CSRF-TOKEN": csrfToken.val()},
+          success: (response, status) => {
+            // console.log("POST login successful")
+            // console.log(response);
+          },
+          error: (error) => {
+            console.log('POST login failed');
+            console.log(error);
+          }
+        });
+      }
+    },
+    error: (error) => {
+      console.log("login get request failed")
+      console.log(error);
     }
-  }).catch( (error) => {
-    console.log("login get request failed")
-    console.log(error);
   });
 }
 
-export const devLogin = login();
+// This function auto executes.
+export const devLogin = function () {
+  if (process.env.NODE_ENV === 'development') {
+    return login();
+  } else {
+    return {};
+  }
+}();
 
 export const devAxios = axios.create({
   baseURL: baseURL(),
