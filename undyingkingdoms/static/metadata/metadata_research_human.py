440782039bde7b1403faddf30ec47c31ebccc27a
elthran <jacobbrunner@gmail.com>
2019-02-28 17:04:43 -0800

    Increased all upkeep costs

--- undyingkingdoms/static/metadata/metadata_research_human.py
+++ undyingkingdoms/static/metadata/metadata_research_human.py
@@ -1,8 +1,8 @@
 from undyingkingdoms.models.technologies import Technology
 
-human_technology = {'human ?': Technology(name='human ?',
-                                          required=75000,
-                                          tier=1,
-                                          max_level=1,
-                                          description='(NOT YET IMPLEMENTED)')
+human_technology = {'civic duty': Technology(name='civic duty',
+                                             required=500,
+                                             tier=1,
+                                             max_level=1,
+                                             description='Reduces upkeep cost of men-at-arms by 5 gold each. (NOT YET IMPLEMENTED)')
                     }
