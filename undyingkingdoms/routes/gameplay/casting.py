c82d900fd582a228f5b20dcd26816100471e178b
Marlen Brunner <klondikemarlen@gmail.com>
2019-03-18 17:15:53 -0700

    Hopefully validate spell targeting.
    Refactor some long queries to their relevant objects.

--- undyingkingdoms/routes/gameplay/casting.py
+++ undyingkingdoms/routes/gameplay/casting.py
@@ -23,19 +23,23 @@ def casting(template, target_id):
         targets = 'hostile'
     else:
         targets = 'all'
-    known_spells = Magic.query.filter_by(county_id=county.id).filter_by(known=True).all()
-    unknown_spells = Magic.query.filter_by(county_id=county.id).filter_by(known=False).all()
-    active_spells = Casting.query.filter_by(county_id=county.id).filter((Casting.duration > 0) | (Casting.active==True)).all()
-    enemy_spells = Casting.query.filter_by(target_id=county.id).filter((Casting.county_id != county.id) & ((Casting.duration > 0) | (Casting.active==True))).all()
+    known_spells = Magic.get_know_spells(county)
+    unknown_spells = Magic.get_unknown_spells(county)
+    active_spells = Casting.get_active_spells(county)
+    enemy_spells = Casting.get_enemy_spells(county)
     casting_history = Casting.query.filter_by(county_id=county.id).all()
-    sustain_mana_requirement = sum(spell.mana_sustain for spell in active_spells)
-    return render_template(template, target=target_county, targets=targets,
-                           known_spells=known_spells,
-                           unknown_spells=unknown_spells,
-                           active_spells=active_spells,
-                           enemy_spells=enemy_spells,
-                           casting_history=casting_history,
-                           sustain_mana_requirement=sustain_mana_requirement)
+    sustain_mana_requirement = Casting.get_sustain_mana_requirement(county)
+    return render_template(
+        template,
+        target=target_county,
+        targets=targets,
+        known_spells=known_spells,
+        unknown_spells=unknown_spells,
+        active_spells=active_spells,
+        enemy_spells=enemy_spells,
+        casting_history=casting_history,
+        sustain_mana_requirement=sustain_mana_requirement
+    )
 
 
 @app.route('/gameplay/cast_spell/<int:spell_id>/<int:target_id>', methods=['GET', 'POST'])
@@ -44,18 +48,26 @@ def cast_spell(spell_id, target_id):
     county = current_user.county
     target = County.query.get(target_id)
     spell = Magic.query.get(spell_id)
-    if spell is None or target is None or spell.mana_cost > county.mana:
+
+    if county == target:
+        target_relation = 'self'
+    elif target.kingdom in county.kingdom.allies:
+        target_relation = 'friendly'
+    else:
+        target_relation = 'hostile'
+
+    if (spell is None
+            or target is None
+            or spell.mana_cost > county.mana
+            or target_relation != spell.targets
+            or not spell.known):
         return redirect(url_for('casting', target_id=target.id))
     county.mana -= spell.mana_cost
 
     cast = Casting(county.id, target.id, spell.id, county.kingdom.world.day, county.day, spell.name, spell.duration)
+    cast.target_relation = target_relation
     cast.save()
-    if county == target:
-        cast.target_relation = 'self'
-    elif target.kingdom in county.kingdom.allies:
-        cast.target_relation = 'friendly'
-    else:
-        cast.target_relation = 'hostile'
+
     if spell.mana_sustain > 0:
         cast.active = True
         cast.mana_sustain = spell.mana_sustain
@@ -65,10 +77,11 @@ def cast_spell(spell_id, target_id):
     elif cast.name == 'summon golem':
         pass
     elif cast.name == 'plague winds':
-        notification = Notification(target.id,
-                                    "Enemy magic", "A plague wind has been summoned by the wizards of {}".format(county.name),
-                                    county.kingdom.world.day,
-                                    "Magic")
+        notification = Notification(
+            target.id,
+            "Enemy magic", "A plague wind has been summoned by the wizards of {}".format(county.name),
+            county.kingdom.world.day,
+            "Magic")
         notification.save()
         pass
     return redirect(url_for('casting', target_id=target.id))
@@ -77,12 +90,13 @@ def cast_spell(spell_id, target_id):
 @app.route('/gameplay/cancel_spell/<int:spell_id>', methods=['GET', 'POST'])
 @login_required
 def cancel_spell(spell_id):
+    county = current_user.county
     spell = Casting.query.get(spell_id)
-    if spell is None:
-        return redirect(url_for('casting', target_id=current_user.county.id))
+    if spell is None or county.id != spell.county_id:
+        return redirect(url_for('casting', target_id=county.id))
     spell.active = False
     spell.duration = 0
-    return redirect(url_for('casting', target_id=current_user.county.id))
+    return redirect(url_for('casting', target_id=county.id))
 
 
 @app.route('/gameplay/dispel_spell/<int:spell_id>', methods=['GET', 'POST'])
@@ -90,7 +104,9 @@ def cancel_spell(spell_id):
 def dispel_spell(spell_id):
     county = current_user.county
     spell = Casting.query.get(spell_id)
-    if spell is None or county.mana < 10:
+    if (spell is None
+            or county.mana < 10
+            or spell.target_id != county.id):
         return redirect(url_for('casting', target_id=current_user.county.id))
     spell.active = False
     spell.duration = 0
