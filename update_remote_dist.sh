bcf13cfa2774bdfbe4fe5a88e63d0f1a851636bf
Marlen Brunner <klondikemarlen@gmail.com>
2019-03-19 17:01:33 -0700

    Update front end data transfer to server script.

--- update_remote_dist.sh
+++ update_remote_dist.sh
@@ -1,9 +1,11 @@
 HOST="undyingkingdoms@ssh.pythonanywhere.com"
 ssh $HOST '
 rm -r ~/UndyingKingdoms/undyingkingdoms/static/dist
+rm -r ~/UndyingKingdoms/undyingkingdoms/templates/dist
 exit
 '
 scp -r << EOF
   undyingkingdoms/static/dist $HOST:~/UndyingKingdoms/undyingkingdoms/static
+  undyingkingdoms/templates/dist $HOST:~/UndyingKingdoms/undyingkingdoms/templates
 EOF
 
