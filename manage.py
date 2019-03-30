30f3ca12daae362823648423bd48744b5be9ea7d
Marlen Brunner <klondikemarlen@gmail.com>
2019-03-30 16:36:03 -0700

    Fixes #179, chat should now have max size and auto-refocusing.
    Squashed commit of the following:
    
    commit 853f3e2b240d840de775a296eccff28d633daf20
    Author: Marlen Brunner <klondikemarlen@gmail.com>
    Date:   Sat Mar 30 16:34:00 2019 -0700
    
        Update chat to allow hiding content.
    
        Chat now magically scrolls into correct view (in theory).
    
    commit 0832024fba5a8cd67a604a22f729a79129253e4c
    Author: Marlen Brunner <klondikemarlen@gmail.com>
    Date:   Sat Mar 30 14:41:09 2019 -0700
    
        Update manage.py file to make it more efficient.

--- manage.py
+++ manage.py
@@ -1,14 +1,7 @@
-import pytest
-
 from flask_script import Manager, Command
-from sqlalchemy import create_engine
 
-from sqlalchemy_utils import database_exists, create_database
-
-import private_config
 from extensions import flask_db as db
 from undyingkingdoms import app
-from utilities.testing_objects import build_testing_objects
 
 manager = Manager(app)
 
@@ -29,6 +22,7 @@ class TestCommand(Command):
     capture_all_args = True
 
     def run(self, command=None):
+        import pytest
         pytest.main(['tests'] + command)
 
 
@@ -53,6 +47,8 @@ def drop_db():
 @manager.command
 def populate_db():
     """Add data to the database."""
+    from utilities.testing_objects import build_testing_objects
+
     with app.app_context():
         # Create the game world
         build_testing_objects()
@@ -64,14 +60,11 @@ def populate_db():
 @manager.command
 def reset_db():
     """Drops then rebuilds the database."""
-    # os.system('mysql --defaults-file=private_mysql_config.cnf -e "DROP DATABASE IF EXISTS {name};"'.format(
-    #         name=private_config.DATABASE_NAME))
-    #     print("Database deleted!")
-    #     os.system(
-    #         'mysql --defaults-file=private_mysql_config.cnf -e '
-    #         '"CREATE DATABASE IF NOT EXISTS {name} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"'.format(
-    #             name=private_config.DATABASE_NAME))
-    #     print("Database recreated!")
+    from sqlalchemy import create_engine
+    from sqlalchemy_utils import database_exists, create_database
+
+    import private_config
+
     engine = create_engine(private_config.SQLALCHEMY_DATABASE_URI)
     if not database_exists(engine.url):
         create_database(engine.url)
