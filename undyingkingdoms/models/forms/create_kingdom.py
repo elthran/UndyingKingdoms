9b6236e841b4e0f6ce63729ff21d65ec9424b223
Marlen Brunner <klondikemarlen@gmail.com>
2019-04-16 14:02:33 -0700

    Bug fix kingdom naming.

--- undyingkingdoms/models/forms/create_kingdom.py
+++ undyingkingdoms/models/forms/create_kingdom.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Length, ValidationError
 
+MAX_WORD_LEN = 24
 
 class CreateKingdomForm(FlaskForm):
-    name = StringField('Name')
+    name = StringField('Name', validators=[
+        DataRequired(message="You must provide a kingdom name."),
+        Length(min=1, max=100),
+    ])
+
+    def validate_name(form, field):
+        if any(len(word) > MAX_WORD_LEN for word in field.data.split()):
+            raise ValidationError(f'No word can be longer than {MAX_WORD_LEN} characters')
