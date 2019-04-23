31e340b7bef9f70dde74e9d33ebabd13e7e69258
Marlen Brunner <klondikemarlen@gmail.com>
2019-04-23 13:38:27 -0700

    Add confirmation dialogue to bash scripts.

--- bin/dist_from_remote.sh
+++ bin/dist_from_remote.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+echo "Do you wish to update your local dist files with files from the server?"
+echo "Type a number then press enter."
+select yn in "Yes" "No"; do
+	case $yn in
+	    Yes ) break;;
+	    No ) exit;;
+	esac
+done
+
+
 HOST="undyingkingdoms@ssh.pythonanywhere.com"
 UDK="undyingkingdoms"
 
