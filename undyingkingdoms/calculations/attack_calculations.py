f5257dd57186d056d2faa21addacfe13784a3e00
Marlen Brunner <klondikemarlen@gmail.com>
2019-04-28 12:25:22 -0700

    Rename file as it kept showing up in my search.

--- undyingkingdoms/calculations/attack_calculations.py
+++ undyingkingdoms/calculations/attack_calculations.py
@@ -1,10 +1,9 @@
 from random import uniform
 
 
-def get_attack_power(county_id, army):
+def get_attack_power(county, army):
     """Returns the attack power of your army"""
     strength = 0
-    county = County.query.get(county_id)
     for name, amount in army.items():
         strength += county.armies.values()[name].attack * amount
     strength *= uniform(0.85, 1.15)
