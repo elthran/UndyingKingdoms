963d0f2cc8e8cb9edd26cc698d33eb6655815f77
Marlen Brunner <klondikemarlen@gmail.com>
2019-05-04 13:36:30 -0700

    Tweak to allow for non-first level techs.
    You should now be able to create
    Technology(
       ...
       tier=2
       max_level=2
    )
    and have no extra tech generated.

--- undyingkingdoms/static/metadata/research/metadata_research_all.py
+++ undyingkingdoms/static/metadata/research/metadata_research_all.py
@@ -54,12 +54,20 @@ generic_technology = [
 
 def generate_tech_levels(techs, requirements):
     """Generate each level up tech up to max level.
+
+    You should now be able to create
+    Technology(
+       ...
+       tier=2
+       max_level=2
+    )
+    and have no extra tech generated.
     """
     all_techs = []
     all_reqs= {}
     for tech in techs:
         all_techs.append(tech)
-        for level in range(1, tech.max_level):
+        for level in range(tech.tier, tech.max_level):
             next_level_tech = deepcopy(tech)
             next_level_tech.tier = tech.tier + level
             next_level_tech.name = romanize(tech.name, next_level_tech.tier)
