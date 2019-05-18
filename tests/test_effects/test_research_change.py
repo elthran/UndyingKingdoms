6ff961b96aa162945fac56fee262a2bc7a437a9d
Marlen Brunner <klondikemarlen@gmail.com>
2019-05-18 11:30:37 -0700

    Try and fail to merge @ethran and my tests.

--- tests/test_effects/test_research_change.py
+++ tests/test_effects/test_research_change.py
@@ -3,18 +3,36 @@ from undyingkingdoms.models.counties.counties import County
 
 def test_alchemy_technology_effects(ctx):
     county = County.query.filter_by(leader="Elthran").one()
+    scientist = county.scientist
 
-    assert county.research_change == 0
+    research_change = scientist.research_change
+    assert scientist.research_change == research_change
 
-    county.technologies['basic alchemy'].completed = True
+    tech = county.technologies['basic alchemy']
+    tech.completed = True
 
-    assert county.research_change == 10
+    expected_change = 0
+    # I should add this code to effects class?
+    for effect in tech.effects:
+        expected_change += effect.kwargs.get('research_change', 0)
 
-    county.technologies['basic alchemy ii'].completed = True
+    assert scientist.research_change > research_change
+    assert scientist.research_change == research_change + expected_change
 
-    assert county.research_change == 20
+    #
+    # assert county.research_change == 0
+    #
+    # county.technologies['basic alchemy'].completed = True
+    #
+    # assert county.research_change == 10
+    #
+    # county.technologies['basic alchemy ii'].completed = True
+    #
+    # assert county.research_change == 20
+    #
+    # county.technologies['basic alchemy ii'].completed = False
+    # county.technologies['basic alchemy'].completed = False
+    #
+    # assert county.research_change == 0
 
-    county.technologies['basic alchemy ii'].completed = False
-    county.technologies['basic alchemy'].completed = False
 
-    assert county.research_change == 0
