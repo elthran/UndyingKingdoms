b713bdb28e57ed532ab83d01efac51d40cf92aee
elthran <jacobbrunner@gmail.com>
2019-05-18 11:46:28 -0700

    Fixed alchemy test

--- tests/test_effects/test_research_change.py
+++ tests/test_effects/test_research_change.py
@@ -3,16 +3,22 @@ from undyingkingdoms.models.counties.counties import County
 
 def test_alchemy_technology_effects(ctx):
     county = County.query.filter_by(leader="Elthran").one()
+
+    # A county with no labs should get 0 research a turn
+    county.buildings['lab'].total = 0
+    assert county.scientist.research_change == 0
+
+    # A county with one lab should get the lab output of research a turn
+    county.buildings['lab'].total = 1
+    assert county.scientist.research_change == county.buildings['lab'].output
+
     scientist = county.scientist
-
     research_change = scientist.research_change
-    assert scientist.research_change == research_change
 
+    # Now that the county has alchemy, it should add the alchemy value
     tech = county.technologies['basic alchemy']
     tech.completed = True
-
     expected_change = 0
-    # I should add this code to effects class?
     for effect in tech.effects:
         expected_change += effect.kwargs.get('research_change', 0)
 
