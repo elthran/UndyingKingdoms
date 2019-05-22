6d6f4b9f161bb0e6f3693a1d06e17c2b34672da1
Marlen Brunner <klondikemarlen@gmail.com>
2019-05-22 16:40:53 -0700

    Prevent verify from resending email on post.
    The email still gets resent if you reload the page or press resend.
    Suppress bug in copy/paste that was adding an a leading space to the key.

--- undyingkingdoms/routes/index/activate.py
+++ undyingkingdoms/routes/index/activate.py
@@ -1,9 +1,10 @@
-from flask import url_for, redirect, render_template
+from flask import url_for, redirect, render_template, request
 from flask_login import current_user, login_required
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 import private_config
+from tests import bp
 from undyingkingdoms import app
 from undyingkingdoms.models.forms.activate import EmailVerificationForm
 
@@ -13,6 +14,13 @@ from undyingkingdoms.models.forms.activate import EmailVerificationForm
 def activate():
     form = EmailVerificationForm()
     user = current_user
+    if request.method == "POST":
+        if form.validate_on_submit():
+            if user.verify_verification_hash(form.code.data.strip()):
+                user.is_verified = True
+                user.save()
+        return redirect(url_for('initialize'))
+
     # TODO: make this only send once unless user request resend.
     # TODO: hash should be different every time.
 
@@ -42,14 +50,10 @@ def activate():
     try:
         sg = SendGridAPIClient(api_key=private_config.SENDGRID_API_KEY)
         # noinspection PyUnresolvedReferences
-        sg.send(message)
+        response = sg.send(message)
+        assert response.status_code == 202
     except Exception as e:
         print(e, message)
-
-    if form.validate_on_submit():
-        if user.verify_verification_hash(form.code.data):
-            user.is_verified = True
-            return redirect(url_for('initialize'))
     return render_template(
         'index/activate.html',
         form=form,
