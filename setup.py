76ae8aeaf159d7229df6d195da211218d8c4a7df
Marlen Brunner <klondikemarlen@gmail.com>
2019-07-16 13:28:24 -0700

    Add module to make `flask shell` run using IPython.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
+        'flask-shell-ipython',
         'flask-sqlalchemy',
         'mysqlclient',
         'flask-wtf',
