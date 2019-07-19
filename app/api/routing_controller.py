85396e741cfaaec86a3df6b24d9f23393e6228e5
Marlen Brunner <klondikemarlen@gmail.com>
2019-07-19 13:12:51 -0700

    Convert Routing controller to Python style naming.
    This works because I just built auto-conversion to JS naming conventions
    into the controller architecture.

--- app/api/routing_controller.py
+++ app/api/routing_controller.py
@@ -1,4 +1,4 @@
-from flask import jsonify, url_for, request
+from flask import url_for, request
 from werkzeug.routing import BuildError
 
 from lib.base_controller import CRUDMethodView
@@ -7,20 +7,20 @@ from lib.base_controller import CRUDMethodView
 class RoutingController(CRUDMethodView):
     def read(self, route):
         if route == 'base':
-            return jsonify(
-                debugMessage="You requested the base url from the routing api.",
+            return dict(
+                debug_message="You requested the base url from the routing api.",
                 base=request.url_root,
             ), 200
         try:
             path = url_for(route, **request.args)
-            return jsonify(
-                debugMessage="You called on the routing api.",
+            return dict(
+                debug_message="You called on the routing api.",
                 base=request.url_root,
                 path=path,
                 # drop leading /
                 url=request.url_root + path[1:]
             ), 200
         except BuildError as ex:
-            return jsonify(
-                debugMessage=str(ex)
+            return dict(
+                debug_message=str(ex)
             ), 404
