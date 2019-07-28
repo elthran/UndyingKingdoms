385ef9c0aa2100628c02bf66d42a6f9ce4fa504f
Marlen Brunner <klondikemarlen@gmail.com>
2019-07-28 16:38:06 -0700

    Attempt to fix my view generator function.
    NOTE: the tests aren't complete, they are just stubs!

---
+++ tests/lib/controller/fake_controller.py
@@ -0,0 +1,23 @@
+from flask import jsonify
+
+from lib.base_controller import CRUDMethodView
+
+
+class FakeController(CRUDMethodView):
+    def string_response(self):
+        return 'foo'
+
+    def string_with_status_response(self):
+        return 'bar', 201
+
+    def dictionary_response(self):
+        return dict(foo='foo')
+
+    def dictionary_with_status_response(self):
+        return dict(foo='foo'), 201
+
+    def json_response(self):
+        return jsonify(bar='bar')
+
+    def json_with_status_response(self):
+        return jsonify(bar='bar'), 201
