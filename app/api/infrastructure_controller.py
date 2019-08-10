5804aa6e9894ed1351e77ca638f58a7c30603ef6
Marlen Brunner <klondikemarlen@gmail.com>
2019-08-10 14:35:26 -0700

    Linked up resource header for infrastructure page.

--- app/api/infrastructure_controller.py
+++ app/api/infrastructure_controller.py
@@ -1,15 +1,22 @@
+from flask_login import current_user, login_required
+
 from lib.base_controller import CRUDMethodView
 
 
 class InfrastructureController(CRUDMethodView):
-    def create(self):
-        pass
-
+    @login_required
     def read(self):
-        pass
+        county = current_user.county
+        infrastructure = county.infrastructure
+
+        return dict(
+            land_available=infrastructure.get_available_land(),
+            land=county.land,
+            citizens_available=county.get_available_workers(),
+            citizens=county.population,
+            citizens_needed=infrastructure.get_workers_needed_to_be_efficient(),
+            efficiency=infrastructure.building_efficiencies(),
+        ), 200
 
     def update(self):
-        pass
-
-    def delete(self):
         pass
--- frontend/src/infrastructure/ResourceHeader.vue
+++ frontend/src/infrastructure/ResourceHeader.vue
@@ -1,14 +1,22 @@
 <template lang="pug">
   #resource-header
     h2.
-      Available Land:
-       {{ availableLand }}&nbsp;/&nbsp;{{ land }}
+      Land Available:
+       {{ landAvailable }}&nbsp;/&nbsp;{{ land }}
     h2.
       Citizens Available:
-       {{ availableCitizens }}&nbsp;/&nbsp;{{ population }}
+       {{ citizensAvailable }}&nbsp;/&nbsp;{{ citizens }}
+    h2(
+      :class="fullEfficiency ? 'positive' : 'negative'"
+    ) Your buildings are currently {{ efficiencyAsPercent }}% efficient.
+      div(
+        v-if="!fullEfficiency"
+      ) You need {{ citizensNeeded }} more citizens to fully utilize your infrastructure.
 </template>
 
 <script>
+import apiPaths from '@/assets/api-paths'
+
 export default {
   name: 'ResourceHeader',
   components: {
@@ -17,14 +25,24 @@ export default {
   },
   data () {
     return {
-      availableLand: -1,
-      availableCitizens: -1,
+      landAvailable: -1,
+      citizensAvailable: -1,
       land: -1,
-      population: -1,
+      citizens: -1,
+      citizensNeeded: -1,
+      efficiency: -1,
+    }
+  },
+  computed: {
+    fullEfficiency () {
+      return this.citizensNeeded === 0
+    },
+    efficiencyAsPercent () {
+      return Math.floor(this.efficiency * 100)
     }
   },
   mounted () {
-    this.$hydrate('/api/infrastructure/resources')
+    this.$hydrate(apiPaths.infrastructure())
     .then(() => {
       this.$emit('loaded')
     })
@@ -33,12 +51,14 @@ export default {
 </script>
 
 <style scoped>
-#resource-header {
+@media(max-width: 640px) {
+  #resource-header {
     max-width: 19em;
     margin: auto;
+  }
 }
 
 h2 {
-    margin-bottom: 0.3em;
+  margin-bottom: 0.3em;
 }
 </style>
