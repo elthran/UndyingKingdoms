0571a1a1cd1877bebc5283534c5f550489d9b961
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 00:33:52 +0000

    Adding tarfile member sanitization to extractall()

--- app/blueprints/GeoIP/__init__.py
+++ app/blueprints/GeoIP/__init__.py
@@ -93,7 +93,26 @@ def update_geo_ip_database():
         # Next extract and replace old data with new data.
         try:
             with tarfile.open(tmp_tar_gz, "r:gz") as tar:
-                tar.extractall('/tmp/geo_ip/')
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar, "/tmp/geo_ip/")
                 os.remove(tmp_tar_gz)
         except FileNotFoundError:
             pass
